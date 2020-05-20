b43a6761be16653fe850967974f2dc56484e3b85
Raif Ahmed <rahmed@redhat.com>
2020-05-20 20:14:31 +0200

    added certificate generation using ansible

--- ansible/sample_run.cmd
+++ ansible/sample_run.cmd
@@ -3,11 +3,11 @@ To configure first ocp cluster
 
 Ensure SSL certificates is generated, for sample self signed you can use create_demo_ssl.sh
 
-export K8S_AUTH_HOST="https://api.cluster-91f1.91f1.sandbox171.opentlc.com:6443"
-export K8S_AUTH_API_KEY="4GdcMJcGbW2QdJEE9BReuEY4uPvBuYkndb3n_3VIq4I"
+export K8S_AUTH_HOST="https://api.cluster-63ff.63ff.sandbox828.opentlc.com:6443"
+export K8S_AUTH_API_KEY="BPk9Vn2for5pbCZArps-AGfDhWl5kSv21-V3JRf0fkY"
 export K8S_AUTH_VERIFY_SSL=no
 
-ansible-playbook install.yml -e amqonline_app_messaging_route_url="demo-app-messaging.apps.cluster-91f1.91f1.sandbox171.opentlc.com" -e amqonline_app_certificate_file_name="messaging-endpoint_cluster-91f1"
+ansible-playbook install.yml -e amqonline_app_messaging_route_url="demo-app-messaging.apps.cluster-63ff.63ff.sandbox828.opentlc.com" -e amqonline_app_certificate_file_name="messaging-endpoint_cluster-63ff"
 
 
 -------------------------------------
@@ -18,8 +18,8 @@ Ensure SSL certificates is generated and also trust the first cluster certificat
 for sample self signed you can use create_demo_ssl_with_remote_trust.sh
 
 
-export K8S_AUTH_HOST="https://api.cluster-e483.e483.sandbox1287.opentlc.com:6443"
-export K8S_AUTH_API_KEY="yBwfOM1zrvkflVErZ5bTbyh_cSjgjz3xw_lr8Yr75Mo"
+export K8S_AUTH_HOST="https://api.cluster-3b4d.3b4d.sandbox893.opentlc.com:6443"
+export K8S_AUTH_API_KEY="467Ysp5rtzerxFyoQSo4B53QfZvhIWTn4VAC-p66d5M"
 export K8S_AUTH_VERIFY_SSL=no
 
-ansible-playbook install.yml -e amqonline_app_messaging_route_url="demo-app-messaging.apps.cluster-e483.e483.sandbox1287.opentlc.com" -e amqonline_app_certificate_file_name="messaging-endpoint_cluster-e483" -e amqonline_app_remote_deploy="yes" -e amqonline_app_remote_messaging_route_url="demo-app-messaging.apps.cluster-91f1.91f1.sandbox171.opentlc.com" -e amqonline_app_remote_certificate_file_name="messaging-endpoint_cluster-91f1"
+ansible-playbook install.yml -e amqonline_app_messaging_route_url="demo-app-messaging.apps.cluster-3b4d.3b4d.sandbox893.opentlc.com" -e amqonline_app_certificate_file_name="messaging-endpoint_cluster-3b4d" -e amqonline_app_remote_deploy="yes" -e amqonline_app_remote_messaging_route_url="demo-app-messaging.apps.cluster-63ff.63ff.sandbox828.opentlc.com" -e amqonline_app_remote_certificate_file_name="messaging-endpoint_cluster-63ff"
